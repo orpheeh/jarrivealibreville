c700ee94b9039081087a9f2bd9d6668f2c4828a6
orpheeh <norpheehounkponou@gmail.com>
2020-07-11 23:45:52 +0100

    feat cotation management

--- install/installadmin.bash
+++ install/installadmin.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #Récupérer le token du root
-curl -H "Accept: application/json" -H "Content-Type: application/json" -d @root.json https://jarrivealibreville.com/admin/auth/root >> token.txt
+curl -H "Accept: application/json" -H "Content-Type: application/json" -d @root.json http://localhost:3000/admin/auth/root >> token.txt
 
 token=`cat token.txt`
 echo $token
-curl -H "Accept: application/json" -H "Content-Type: application/json" -H "authorization: Bearer ${token}" -d @ditch.json https://jarrivealibreville.com/admin
-curl -H "Accept: application/json" -H "Content-Type: application/json" -H "authorization: Bearer ${token}" -d @rodhes.json https://jarrivealibreville.com/admin
+curl -H "Accept: application/json" -H "Content-Type: application/json" -H "authorization: Bearer ${token}" -d @ditch.json http://localhost:3000/admin
+curl -H "Accept: application/json" -H "Content-Type: application/json" -H "authorization: Bearer ${token}" -d @rodhes.json http://localhost:3000/admin
 
 rm token.txt
 
