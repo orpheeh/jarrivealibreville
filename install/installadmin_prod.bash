0e750a14fd6762a59d4bfadf406cc2aa7c582b4f
orpheeh <norpheehounkponou@gmail.com>
2020-07-25 10:45:57 +0100

    fix post admin 301

--- install/installadmin_prod.bash
+++ install/installadmin_prod.bash
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 #Récupérer le token du root
-curl -H "Accept: application/json" -H "Content-Type: application/json" -d @root.json http://jarrivealibreville.com/admin/auth/root >> token.txt
+curl -H "Accept: application/json" -H "Content-Type: application/json" -d @root.json https://jarrivealibreville.com/admin/auth/root >> token.txt
 
 token=`cat token.txt`
 echo $token
-curl -H "Accept: application/json" -H "Content-Type: application/json" -H "authorization: Bearer ${token}" -d @ditch.json http://jarrivealibreville.com/admin
-curl -H "Accept: application/json" -H "Content-Type: application/json" -H "authorization: Bearer ${token}" -d @rodhes.json http://jarrivealibreville.com/admin
+curl -H "Accept: application/json" -H "Content-Type: application/json" -H "authorization: Bearer ${token}" -d @ditch.json https://jarrivealibreville.com/admin
+curl -H "Accept: application/json" -H "Content-Type: application/json" -H "authorization: Bearer ${token}" -d @rodhes.json https://jarrivealibreville.com/admin
 
 rm token.txt
 
